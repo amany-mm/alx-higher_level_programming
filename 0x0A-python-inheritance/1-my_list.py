80ced04afff657780133d0547a432b3569b9e89a
Amany Mohammed <amany.mohammd@gmail.com>
2024-04-16 09:34:58 +0200

    print instead of return to fix QA review

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,9 +6,5 @@ class MyList(list):
     """ class MyList that inherits from list. """
 
     def print_sorted(self):
-        """print list of integer elements
-
-        Returns:
-            list (int): list of all the elements of type integer.
-        """
-        return sorted(self)
+        """print list of integer elements"""
+        print(sorted(self))
