2c010713f7946fa8c00b08897bf86e911244859e
Amany Mohammed <amany.mohammd@gmail.com>
2024-05-22 03:30:22 +0300

    Format SQL

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -10,9 +10,41 @@
 -- id = 3, name = “Bob”, score = 14
 -- id = 4, name = “George”, score = 8
 -- ubuntu@ubuntu:~/$ cat 9-full_creation.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table
+  (
+     id    INT,
+     name  VARCHAR(256),
+     score INT
+  );
 
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
-INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
-INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
-INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
+INSERT INTO second_table
+            (id,
+             name,
+             score)
+VALUES      (1,
+             'John',
+             10);
+
+INSERT INTO second_table
+            (id,
+             name,
+             score)
+VALUES      (2,
+             'Alex',
+             3);
+
+INSERT INTO second_table
+            (id,
+             name,
+             score)
+VALUES      (3,
+             'Bob',
+             14);
+
+INSERT INTO second_table
+            (id,
+             name,
+             score)
+VALUES      (4,
+             'George',
+             8); 
