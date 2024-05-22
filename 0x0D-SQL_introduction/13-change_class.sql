fd0c4d637a1dfa2701645cd9da52337c73380781
Amany Mohammed <amany.mohammd@gmail.com>
2024-05-22 03:24:55 +0300

    Format SQL

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -2,4 +2,5 @@
 -- in the table second_table of the database hbtn_0c_0 in your MySQL server.
 -- The database name will be passed as an argument of the mysql command
 -- ubuntu@ubuntu:~/$ cat 13-change_class.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-DELETE FROM second_table WHERE score <=5;
+DELETE FROM second_table
+WHERE  score <= 5;
