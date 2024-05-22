3189401a5305aa2752dcc087c5cf15bdfc4bbd3e
Amany Mohammed <amany.mohammd@gmail.com>
2024-05-22 03:18:42 +0300

    Format SQL

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -5,4 +5,8 @@
 -- * The database name will be passed as an argument to the mysql command
 -- In this example, new data have been added to the table second_table.
 -- ubuntu@ubuntu:~/$ cat 16-no_link.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score,
+       name
+FROM   second_table
+WHERE  name IS NOT NULL
+ORDER  BY score DESC;
