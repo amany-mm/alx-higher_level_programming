dbe35e185415aa6a73f711c7577bd05c6d20321e
Amany Mohammed <amany.mohammd@gmail.com>
2024-05-22 03:33:36 +0300

    Format SQL

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -3,4 +3,5 @@
 -- All fields should be printed
 -- The database name will be passed as an argument of the mysql command
 -- ubuntu@ubuntu:~/$ cat 6-list_values.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-SELECT * FROM first_table;
+SELECT *
+FROM   first_table;
