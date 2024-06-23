a1ff83d89a8c096f5c3551c9239fb3ce8c522e16
Amany Mohammed <amany.mohammd@gmail.com>
2024-06-23 14:08:06 +0300

    Add a script that lists all states from the database hbtn_0e_0_usa.

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" A script that lists all states from the database hbtn_0e_0_usa """
+
+from sys import argv
+import MySQLdb
+
+
+def main():
+    """ Function that connects to MySQL server on localhost at port 3306 """
+
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to exec queries using SQL
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
