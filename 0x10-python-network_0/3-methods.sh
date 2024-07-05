c0c00dcdec3529b377cc6e7c4f797f0838d25fdf
Amany Mohammed <amany.mohammd@gmail.com>
2024-07-05 03:11:18 +0300

    Try to fix checker of task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes URL and displays all HTTP methods server will accept; Usage: ./3-methods.sh 0.0.0.0:5000/route_4
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+curl -si -X OPTIONS "$1" | grep "Allow" | cut -c 8-
