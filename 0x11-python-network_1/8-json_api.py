bc7a4bcb3a680bdbc4e6170386ee6acd15ee6e4a
Amany Mohammed <amany.mohammd@gmail.com>
2024-07-06 05:25:17 +0300

    8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+Usage: ./8-json_api.py [letter only] e.g. ./8-json_api.py a
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        letter = ""
+    else:
+        letter = argv[1]
+
+    url = requests.post("http://0.0.0.0:5000/search_user", {'q': letter})
+
+    try:
+        response = url.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+
+    except ValueError:
+        print("Not a valid JSON")
