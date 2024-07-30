0324302f063e05ac45b4f4aac139ddf1c25e0b10
Amany Mohammed <amany.mohammd@gmail.com>
2024-07-31 01:49:28 +0300

    Add script that computes the number of tasks completed by user id.

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+/**
+ * Script that computes the number of tasks completed by user id.
+ * Usage $ ./6-completed_tasks.js https://jsonplaceholder.typicode.com/todos
+*/
+
+const request = require('request');
+
+request(process.argv[2], function (err, _res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const completedTasksByUsers = {};
+    body = JSON.parse(body);
+
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
+      }
+
+      if (completed) ++completedTasksByUsers[userId];
+    }
+
+    console.log(completedTasksByUsers);
+  }
+});
