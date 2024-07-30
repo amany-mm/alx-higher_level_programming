38a7663b934ee05dd875c7a97fa3e5d743a04a61
Amany Mohammed <amany.mohammd@gmail.com>
2024-07-30 23:49:17 +0300

    Add comments

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 /**
- * script that reads and prints the content of a file.
+ * Script that reads and prints the content of a file.
  * Usage $ ./0-readme.js cisfun
 */
 const fileName = process.argv[2];
 const fs = require('fs');
 
-fs.readFile(fileName, 'UTF-8', (error, fileContent) => {
-  if (error) {
-    console.log(error);
+fs.readFile(fileName, 'UTF-8', (err, fileContent) => {
+  if (err) {
+    console.log(err);
   } else {
     console.log(fileContent);
   }
